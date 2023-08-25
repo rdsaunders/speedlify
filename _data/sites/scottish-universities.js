4f4cbe8af47f9201c7459401857ab542be7c8555
Richard Saunders <r.saunders@zengenti.com>
2023-08-25 16:39:06 +0100

    Remove skip

--- _data/sites/scottish-universities.js
+++ _data/sites/scottish-universities.js
@@ -2,8 +2,6 @@ module.exports = {
 	name: "Scottish Unis", // optional, falls back to object key
 	description: "Scottish University Sites",
 	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
